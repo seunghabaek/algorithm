c1a261e534c340ee6afb5ef4951220c295d6f295
seungha <slug0326@naver.com>
2021-08-09 00:38:54 +0900

    Finish dfs bfs algorithm

--- dfs_bfs.py
+++ dfs_bfs.py
@@ -153,5 +153,70 @@ def dfs(x, y):
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
     # 현재 노드를 아직 방문하지 않았다면
-    if graph[x][y] = 0:
+    if graph[x][y] == 0:
         graph[x][y] = 1
+        # 상, 하, 좌, 우 방문처리
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+# input
+n, m = map(int, input().split())
+
+# 2차원 리스트
+graph = []
+for i in range(n):
+    graph.append(list(map(int,input)))
+    
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        # 현재 위치에서 dfs 실행
+        if dfs(i, j) == True:
+            result += 1
+    
+print(result)
+        
+#%%
+# 미로 탈출 문제
+from collections import deque
+
+# N, M 가져옴
+n, m = map(int, input().split())
+
+# 지도 가져옴
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+    
+# 이동할 네가지 방향 초기화
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# bfs 코드 구현
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    # 큐가 빌때까지 반복
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 4가지 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로 공간 범위
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 벽인 경우 무시(괴물)
+            if graph[x][y] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] == graph[x][y] + 1
+                queue.append(nx, ny)
+# 가장 오른쪽 아래까지의 최단 거리 반환
+return graph[n - 1][m - 1]
