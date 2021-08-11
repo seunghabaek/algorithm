3a09ae1c1f20bd4c219faa5243957124f4141c05
seungha <slug0326@naver.com>
2021-08-11 23:53:07 +0900

    Finish quick algorithm

--- sort algorithm.py
+++ sort algorithm.py
@@ -41,3 +41,45 @@ print(a)
 
 #%%
 # 퀵 정렬 알고리즘
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array, start, end):
+    if start >= end: # 원소의 갯수가 1일때
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while (left <= right):
+        # 피벗보다 큰 데이터 찾기
+        while (left <= end and array[left] <= array[pivot]):
+            left += 1
+        while (right > start and array[right] >= array[pivot]):
+            right -= 1
+        # 왼쪽 오른쪽이 엇갈리는 경우
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[right], array[left] = array[left], array[right]
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1, end)
+
+quick_sort(array, 0, len(array) - 1)
+
+# list comprehensing이용
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    pivot = array[0] # pivot
+    tail = array[1:] # without pivot list
+    
+    left_side = [x for x in tail if x <= pivot] # left side
+    right_side = [x for x in tail if x > pivot] # rght side
+    
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
+
+#%%
+# 계수 정렬 알고리즘
