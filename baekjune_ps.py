10416c6d3279f6e78a47d6a03681034783d29c4c
seungha <slug0326@naver.com>
2021-08-19 00:46:55 +0900

    Add ~2193

--- baekjune_ps.py
+++ baekjune_ps.py
@@ -400,16 +400,77 @@ for i in range(1, n+1):
 print(dp[n]%10007)
 
 #%%
-# num 9095
+# num 9095 - 1,2,3으로 숫자 만들기
 
-# num 10844
+def fibo_3(alpha):
+    dp = [0] * (alpha+1)
+    for j in range(1, alpha+1):
+        if j == 1:
+            dp[j] = 1
+        elif j == 2:
+            dp[j] = 2
+        elif j == 3:
+            dp[j] = 4
+        else:
+            dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
+    return dp[alpha]
 
-# num 11057
+n = int(input())
+a = list(int(input()) for i in range(n))
 
-# num 2193
+for i in range(n):
+    print(fibo_3(a[i]))
 
+#%%
+# num 10844 - 계단 수 구하기
+
+n = int(input())
+dp = [[0]*10 for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(10):
+        if (i == 1 and j >= 1):
+            dp[i][j] = 1
+        elif i >= 2:
+            if j == 0:
+                dp[i][j] = dp[i-1][j+1]
+            elif j == 9:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = dp[i-1][j+1] + dp[i-1][j-1]
+
+print((sum(dp[n]))%1000000000)
+
+#%%
+# num 11057 - 오르막 수 구하기
+
+n = int(input())
+dp = [[0] * 10 for _ in range(n+1)]
+
+dp[1] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] # 미리 define 해주는게 빠름.
+
+for i in range(2, n+1):
+    for j in range(0, 10):
+        dp[i][j] = sum(dp[i-1][0:j+1]) # list slicing
+
+print(sum(dp[n])%10007)
+
+#%%
+# num 2193 - pinary number(특수한 이진수)
+
+n = int(input())
+dp = [1] * (n+1) # dp[1], dp[2] 둘다 1이라서 가능.
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+    
+print(dp[n])
+
+#%%
 # num 9466
 
+
+#%%
 # num 2156
 
 # num 11053
